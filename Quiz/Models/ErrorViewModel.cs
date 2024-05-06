69b2b1618b23a4a69c25b9a79bd31a32311858d1
Kevin Finn <101456324+kev-n14@users.noreply.github.com>
2024-05-06 13:07:27 +0100

    Delete Quiz directory
    Deleted this project

--- Quiz/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Quiz.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
