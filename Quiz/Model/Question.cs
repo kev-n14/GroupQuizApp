2b1b7ea9deed628f3f102b73990b6c0f8d6c276b
Kevin Finn <101456324+kev-n14@users.noreply.github.com>
2024-05-06 13:12:22 +0100

    Added Quiz Project
    Added project with a Model file.

---
+++ Quiz/Model/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Model
+{
+        public class Question
+        {
+            public int QuestionId { get; set; }
+            public string Category { get; set; }
+            public DifficultyLevel Difficulty { get; set; }
+            public string QuestionText { get; set; }
+            public int Points { get; set; }
+            public List<Answer> Answers { get; set; }
+        }
+        public enum DifficultyLevel
+        {
+            Easy,
+            Medium,
+            Hard
+        }
+
+        public class Answer
+        {
+            public int AnswerId { get; set; }
+            public string AnswerText { get; set; }
+            public bool IsCorrect { get; set; }
+        }
+    }
+
