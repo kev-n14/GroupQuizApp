45df2ba8aaaa5cb77b9175c14be3018e72695bc1
Kevin Finn <101456324+kev-n14@users.noreply.github.com>
2024-05-06 12:43:08 +0100

    Added Quiz Web App
    I added a base project (ASP.NET Web Core App MVC) with a Model file populated with Properties for Question and Answer Object.

---
+++ Quiz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Quiz/Models/Question.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace Quiz.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string Category { get; set; }
+        public DifficultyLevel Difficulty { get; set; }
+        public string QuestionText { get; set; }
+        public int Points { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+    public enum DifficultyLevel
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        public string AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
