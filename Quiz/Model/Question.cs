ff82f404da981782601537e7f1c1813d333387db
Kevin Finn <101456324+kev-n14@users.noreply.github.com>
2024-05-07 14:42:53 +0100

    Added Category, Type  enums and Locale properties.

--- Quiz/Model/Question.cs
+++ Quiz/Model/Question.cs
@@ -9,24 +9,40 @@ namespace Quiz.Model
         public class Question
         {
             public int QuestionId { get; set; }
-            public string Category { get; set; }
+            public QuestionCategory Category { get; set; }
+            public QuestionType Type { get; set; }
             public DifficultyLevel Difficulty { get; set; }
             public string QuestionText { get; set; }
             public int Points { get; set; }
             public List<Answer> Answers { get; set; }
+            public string Locale { get; set; }
         }
         public enum DifficultyLevel
         {
             Easy,
             Medium,
-            Hard
+            Hard,
+        }
+        public enum QuestionCategory
+        {
+            Science,
+            History,
+            Geography,
+            Movie,
+
+        }
+        public enum QuestionType
+        {
+            SingleAnswer,
+            MultipleChoice,
+            TrueFalse,
         }
 
         public class Answer
-        {
-            public int AnswerId { get; set; }
-            public string AnswerText { get; set; }
-            public bool IsCorrect { get; set; }
-        }
+            {
+                public int AnswerId { get; set; }
+                public string AnswerText { get; set; }
+                public bool IsCorrect { get; set; }
+            }
     }
 
--- Quiz/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
+++ Quiz/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b1b7ea9deed628f3f102b73990b6c0f8d6c276b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
