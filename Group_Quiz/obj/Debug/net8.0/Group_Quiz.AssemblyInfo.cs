754689d9e3d6af5f87c756f44005939a53fad875
Kevin Finn <101456324+kev-n14@users.noreply.github.com>
2024-05-17 13:23:29 +0100

    update navbar added logo
    added logo and fixed placement

--- Group_Quiz/obj/Debug/net8.0/Group_Quiz.AssemblyInfo.cs
+++ Group_Quiz/obj/Debug/net8.0/Group_Quiz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group_Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d8679f4e7e76f93b663de42a45ad7078af231e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511f92d56f233b81ecb97cdff7aec45bfdffd84d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group_Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group_Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
