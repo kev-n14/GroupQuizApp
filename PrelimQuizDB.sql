c6fddb334cff1428c2d4b8a7f359357017fa9cf2
Khaelim <Khaelim@live.ie>
2024-05-30 19:21:52 +0100

    Update PrelimQuizDB.sql

--- PrelimQuizDB.sql
+++ PrelimQuizDB.sql
@@ -1,18 +1,18 @@
 CREATE DATABASE IF NOT EXISTS quiz;
 USE quiz;
 
-##
+--
 CREATE TABLE questions (
     question_id INT PRIMARY KEY AUTO_INCREMENT,
     question_text TEXT NOT NULL,
     question_type ENUM('single', 'multiple', 'true_false') NOT NULL,
-    category VARCHAR(100) NOT NULL, 			#Maybe have this enum once categories are decided
+    category VARCHAR(100) NOT NULL, 			-- Maybe have this enum once categories are decided
     difficulty ENUM('easy', 'medium', 'hard') NOT NULL,
     locale VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-###
+--
 CREATE TABLE answers (
     answer_id INT PRIMARY KEY AUTO_INCREMENT,
     question_id INT NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE answers (
     FOREIGN KEY (question_id) REFERENCES questions(question_id) ON DELETE CASCADE
 );
 
-###
+--
 CREATE TABLE keywords (
     keyword_id INT PRIMARY KEY AUTO_INCREMENT,
     question_id INT NOT NULL,
